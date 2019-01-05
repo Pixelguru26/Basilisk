973ce11ffdc9896ffc92099225106c0b0419085f
AutonomyCOG <autonomycog@gmail.com>
2019-01-04 20:27:34 -0700

    Fixed theme to be labeled and stuff

--- Basilisk/Basilisk/Theme.cs
+++ Basilisk/Basilisk/Theme.cs
@@ -8,37 +8,62 @@ namespace Basilisk
 {
 	public class Theme
 	{
+        // Menu includes any of the following:
+        //  - The Chat Panel
+        //  - Popup Menus
+        //  - Friends Menu
 		[JsonConverter(typeof(HexColorJsonConverter))]
-		public Color Panel;
+		public Color MenuBack;
 		[JsonConverter(typeof(HexColorJsonConverter))]
-		public Color PanelLight;
+		public Color MenuLight;
 		[JsonConverter(typeof(HexColorJsonConverter))]
-		public Color PanelDark;
+		public Color MenuDark;
 		[JsonConverter(typeof(HexColorJsonConverter))]
-		public Color PanelInput;
+		public Color MenuInput;
 		[JsonConverter(typeof(HexColorJsonConverter))]
-		public Color PanelBorder;
+		public Color MenuBorder;
 
+        // Panel includes any of the following:
+        //  - Friend / Server / Channel List
+        //  - Text Input / Button
+        //  - Any Buttons
 		[JsonConverter(typeof(HexColorJsonConverter))]
-		public Color Input;
+		public Color PanelBack;
 		[JsonConverter(typeof(HexColorJsonConverter))]
-		public Color InputBack;
-		[JsonConverter(typeof(HexColorJsonConverter))]
-		public Color InputFore;
+		public Color PanelFore;
+        [JsonConverter(typeof(HexColorJsonConverter))]
+        public Color PanelCaret;
+        [JsonConverter(typeof(HexColorJsonConverter))]
+        public Color PanelBorder;
 
-		[JsonConverter(typeof(HexColorJsonConverter))]
+        // Grounds includes the following:
+        //  - Text Color of any Menus
+        //  - Background Color of any Misc. Menus / The Main Window
+        [JsonConverter(typeof(HexColorJsonConverter))]
 		public Color Background;
 		[JsonConverter(typeof(HexColorJsonConverter))]
 		public Color Foreground;
 
+        // Text includes the following:
+        //  - Color of Text / Text Selection in Chat
 		[JsonConverter(typeof(HexColorJsonConverter))]
 		public Color Text;
 		[JsonConverter(typeof(HexColorJsonConverter))]
 		public Color TextLink;
 		[JsonConverter(typeof(HexColorJsonConverter))]
 		public Color TextUser;
+        [JsonConverter(typeof(HexColorJsonConverter))]
+		public Color TextSystem;
+        [JsonConverter(typeof(HexColorJsonConverter))]
+        public Color TextSelection;
 
-		[JsonConverter(typeof(HexColorJsonConverter))]
+        // Misc. includes the following:
+        //  - Whatever I want, screw you
+        [JsonConverter(typeof(HexColorJsonConverter))]
 		public Color Title;
-	}
+        [JsonConverter(typeof(HexColorJsonConverter))]
+        public Color Subtitle;
+        [JsonConverter(typeof(HexColorJsonConverter))]
+        public Color Tooltip;
+    }
 }
